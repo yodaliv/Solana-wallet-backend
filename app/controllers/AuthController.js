46cdcc7011adc77f69f29476b3f1bb2884e9b928
lievander <lievander24@gmail.com>
2022-02-05 19:20:50 +0100

    feat: update form submit

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -27,17 +27,17 @@ exports.Verify = (req, res) => {
       if(nfts.length) {
           nfts.map((nftData) => {
               if(nftData.updateAuthority === config.CREATOR_WALLET_ADDRESS) {
-
-                var date = new Date();
-                var iv = 'encrypt for aliennews';
-                var key = "12345678123456781234567812345678";
-                var encrypter = crypto.createCipheriv("aes-256-cbc", key, iv.slice(0,16));
-                var decipher = crypto.createDecipheriv('aes-256-cbc', key,iv.slice(0,16));
-                var encryptedMsg = encrypter.update(date.toString(), "utf8", "base64");
-                var decryptedMsg = decipher.update(encryptedMsg, 'base64', 'utf8');
-                res.send({result: 'true', redirectUrl : `https://alientriptales.com/aliennews?message=${encryptedMsg}`});
+                // var date = new Date();
+                // var iv = 'encrypt for aliennews';
+                // var key = "12345678123456781234567812345678";
+                // var encrypter = crypto.createCipheriv("aes-256-cbc", key, iv.slice(0,16));
+                // var decipher = crypto.createDecipheriv('aes-256-cbc', key,iv.slice(0,16));
+                // var encryptedMsg = encrypter.update(date.toString(), "utf8", "base64");
+                // var decryptedMsg = decipher.update(encryptedMsg, 'base64', 'utf8');
+                //res.send({result: 'true', redirectUrl : `https://alientriptales.com/aliennews?message=${encryptedMsg}`});
+                res.send({result: true, message:'Verify sucess'});
               }else{
-                  res.send({result:'false'});
+                res.send({result: false, message:'Sorry. Access denied, you do not have the required NFT.'});
               }
           })
       }
@@ -45,6 +45,6 @@ exports.Verify = (req, res) => {
   }
   else
   {
-    res.send({result: 'verify is failed.'});
+    res.send({result: 'Sorry. Access denied, you do not have the required NFT.'});
   }
 }
