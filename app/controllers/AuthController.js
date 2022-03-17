e69771ca432cce0fcf50d054b1373d7e4ca3ccad
lievander <lievander24@gmail.com>
2022-03-17 21:52:49 +0400

    feat: update some function

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,9 +1,8 @@
 const crypto = require('crypto');
 const nacl = require('tweetnacl');
-const solanaWeb3 = require('@solana/web3.js');
-const TOKEN_PROGRAM_ID = require("@solana/spl-token");
 const getNFT = require("@nfteyez/sol-rayz");
 const base58 = require('bs58');
+
 const config = require("../config/config.js");
 
 exports.getRandomMessage = (req, res) => {
@@ -12,11 +11,10 @@ exports.getRandomMessage = (req, res) => {
 }
 
 exports.Verify = (req, res) => {
-
   const publicKey = base58.decode(req.body.publicKey);
-  //is received from the client signature and publicKey
+  // is received from the client signature and publicKey
   const rdMsg = Object.keys(req.body.randomMessage).map(key => req.body.randomMessage[key]);
-  if(nacl.sign.detached.verify(Uint8Array.from(rdMsg), Uint8Array.from(req.body.signature.data), publicKey))
+  if (nacl.sign.detached.verify(Uint8Array.from(rdMsg), Uint8Array.from(req.body.signature.data), publicKey))
   {
     (async () => {
       const nfts = await getNFT.getParsedNftAccountsByOwner({
@@ -24,19 +22,11 @@ exports.Verify = (req, res) => {
         serialization: true,
       });
       
-      if(nfts.length) {
+      if (nfts.length) {
           nfts.map((nftData) => {
-              if(nftData.updateAuthority === config.CREATOR_WALLET_ADDRESS) {
-                // var date = new Date();
-                // var iv = 'encrypt for aliennews';
-                // var key = "12345678123456781234567812345678";
-                // var encrypter = crypto.createCipheriv("aes-256-cbc", key, iv.slice(0,16));
-                // var decipher = crypto.createDecipheriv('aes-256-cbc', key,iv.slice(0,16));
-                // var encryptedMsg = encrypter.update(date.toString(), "utf8", "base64");
-                // var decryptedMsg = decipher.update(encryptedMsg, 'base64', 'utf8');
-                //res.send({result: 'true', redirectUrl : `https://alientriptales.com/aliennews?message=${encryptedMsg}`});
+              if (nftData.updateAuthority === config.CREATOR_WALLET_ADDRESS) {
                 res.send({result: true, message:'Verify sucess'});
-              }else{
+              } else{
                 res.send({result: false, message:'Sorry. Access denied, you do not have the required NFT.'});
               }
           })
